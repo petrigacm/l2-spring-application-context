acb244f1356d55db5aa0297d8973364f1d261ae6
Richard Forrai <forrairichard11@gmail.com>
2020-09-29 20:25:56 +0200

    clean up

--- src/main/java/sk/stopangin/spring/l2/beans/aware/SomeService.java
+++ src/main/java/sk/stopangin/spring/l2/beans/aware/SomeService.java
@@ -2,14 +2,9 @@ package sk.stopangin.spring.l2.beans.aware;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Service
 public class SomeService implements BeanNameAware, ApplicationContextAware {
